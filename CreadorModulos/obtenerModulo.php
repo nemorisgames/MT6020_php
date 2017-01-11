e60b5aeef491e1eb67923701fd0c5f83860b5b61
njaramillo-ng <njaramillo@nemorisgames.com>
2017-01-11 17:22:21 -0300

    Carpeta php

---
+++ CreadorModulos/obtenerModulo.php
@@ -0,0 +1,21 @@
+﻿<?php
+include("funciones.php");
+   $dbaddress='localhost'; $dbuser='root'; $dbpass=''; $dbname='mt6020';
+   
+$dbcnx = mysql_connect($dbaddress,$dbuser,$dbpass)or die("Could not connect: " . mysql_error());
+mysql_select_db($dbname, $dbcnx) or die ('Unable to select the database: ' . mysql_error());
+
+
+$query=mysql_query("SELECT * FROM Module WHERE id = ".$_POST['id']) or die("-2");
+$salida = "";
+
+while($resultQuery = mysql_fetch_array($query)){
+
+  $salida=$salida.$resultQuery['name']."|".$resultQuery['fk_moduleType']."|".$resultQuery['name']."*";
+
+};
+
+echo $salida;
+
+mysql_close($dbcnx);
+?>
---
+++ CreadorModulos/obtenerNombreTipoModulo.php
@@ -0,0 +1,16 @@
+﻿<?php
+include("funciones.php");
+   $dbaddress='localhost'; $dbuser='root'; $dbpass=''; $dbname='mt6020';
+   
+$dbcnx = mysql_connect($dbaddress,$dbuser,$dbpass)or die("Could not connect: " . mysql_error());
+mysql_select_db($dbname, $dbcnx) or die ('Unable to select the database: ' . mysql_error());
+
+
+$query=mysql_query("SELECT name FROM ModuleType WHERE id = ".$_POST['idTipoModulo']) or die("-2");
+$salida = mysql_result($query, 0);
+
+echo $salida;
+
+mysql_close($dbcnx);
+
+?>
