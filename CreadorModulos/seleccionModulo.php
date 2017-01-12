3b12266448447e60561389197993c3c57bd4a9a9
sleivanemoris <sleivanemoris@nemorisgames.com>
2017-01-12 14:42:13 -0300

    seleccionmodulo

---
+++ CreadorModulos/seleccionModulo.php
@@ -0,0 +1,18 @@
+<?php
+$dbaddress='localhost'; $dbuser='root'; $dbpass=''; $dbname='mt6020';
+
+$dbcnx = mysql_connect($dbaddress,$dbuser,$dbpass)
+or die("Could not connect: " . mysql_error());
+mysql_select_db($dbname, $dbcnx) or die ('Unable to select the database: ' . mysql_error());
+mysql_query("SET NAMES 'utf8'");
+
+$query = mysql_query("SELECT * FROM module");
+echo $query;
+
+$salida="";
+while($resultQuery = mysql_fetch_array($query)){
+	$salida=$salida.$resultQuery['fk_moduleType'].'*'.$resultQuery['name'].'|';
+};
+echo $salida;
+
+?>
