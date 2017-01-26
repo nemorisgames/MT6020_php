54c8157b18c012d5bd2e29c2084f5003a586d47b
sleiva <sleiva@nemorisgames.com>
2017-01-26 13:40:37 -0300

    realizationmodule operacional

---
+++ operationInfoModuleDetail.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+$dbaddress='localhost'; $dbuser='root'; $dbpass=''; $dbname='mt6020';
+
+$dbcnx = mysql_connect($dbaddress,$dbuser,$dbpass)or die("Could not connect: " . mysql_error());
+mysql_select_db($dbname, $dbcnx) or die ('Unable to select the database: ' . mysql_error());
+
+mysql_query("SET NAMES 'utf8'");
+
+$query=mysql_query("INSERT INTO InformationModuleDetail (fk_realizationModule, fk_questionID, fk_informationModuleAnswers) VALUES (3,".$_POST['preguntaID'].",".$_POST['respuestaUsuarioID'].")") or die("-2");
+echo " ".$query;
+
+mysql_close($dbcnx);
+?>
